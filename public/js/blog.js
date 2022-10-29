a13c23fd5f3d25d4d99c56fb71bc731e732b6982
Haruka08 <haruka.hashimoto08@gmail.com>
2022-10-29 14:57:48 +0100

    corrected the source in dashboard.handlebars files

--- public/js/blog.js
+++ public/js/blog.js
@@ -2,6 +2,9 @@ const postBlog = async (event) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     event.preventDefault();
 
+    const title = document.querySelector('#title').value
+    const content = document.querySelector('#content').value
+
     const response = await fetch('/dashboard/blog/new', {
         method: 'POST',
         body: JSON.stringify({ title, content}),
